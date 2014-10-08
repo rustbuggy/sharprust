9383868a1eaec272e0ddba8ca698b78d92bd4bc1
Timo Tomson <timo.tomson@defendec.com>
2014-10-08 06:58:07 +0300

    reflective surface too near is similar to reflective surface too far?

--- firmware/LUT_creation.m
+++ firmware/LUT_creation.m
@@ -7,12 +7,13 @@ clc
 % Sharp IR sensor 10..80 cm
 lookup = zeros(1,1024);
 for i = 1:1024
-    voltFromRaw = (i - 1) * (4999 / 1023) + 1;
-    cm = 27.728 * ((voltFromRaw / 1000) ^ (-1.2045));
-    if cm > 120
-        cm = 120;
+    if i > 64
+        voltFromRaw = (i - 1) * (4999 / 1023) + 1;
+        cm = 27.728 * ((voltFromRaw / 1000) ^ (-1.2045));
+        lookup(1,i) = fix(cm); % to integer
+    else
+        lookup(1,i) = 0;
     end
-    lookup(1,i) = fix(cm); % to integer
 end
 
 % create the lookup source file
@@ -23,7 +24,7 @@ fprintf(fid, 'const uint8_t irLookup[1024] = {\n');
 for y = 1:64
     for x = 1:16
         ind = (y - 1) * 16 + x;
-        fprintf(fid, '%d, ', lookup(1, ind));
+        fprintf(fid, '%3d, ', lookup(1, ind));
     end
     fprintf(fid, '\n');
 end

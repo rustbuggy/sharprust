9576fdd1cd464b6e74e1f9fee519a1a083b4f7f7
Andres Vahter <andres.vahter@gmail.com>
2014-10-16 22:22:51 +0300

    fix __attribute__((packed)) for teensy, change packet headers

--- firmware/communication.h
+++ firmware/communication.h
@@ -1,17 +1,17 @@
-#ifndef __COMMUNICAYION__H
-#define __COMMUNICAYION__H
+#ifndef __COMMUNICATION__H
+#define __COMMUNICATION__H
 
 #include "types.h"
 
 // bc means from Buggy to Computer
 // cb means from Computer tp Buggy
 enum {
-	BC_TELEMETRY = 0x00,
+	BC_TELEMETRY = 0x01,
 
-	CB_MOTOR_COMMAND = 0x01,
+	CB_MOTOR_COMMAND = 0x02,
 };
 
-typedef struct bc_telemetry_packet_t {
+typedef struct __attribute__((packed)) {
 	uint8_t header; // BC_TELEMETRY
 	uint32_t time;
 	fixed_t ir_left;
@@ -28,13 +28,13 @@ typedef struct bc_telemetry_packet_t {
 	int32_t steeringPwm;
 	bool changeDriving;
 	int32_t drivingPwm;
-} bc_telemetry_packet_t __attribute__((packed));
+} bc_telemetry_packet_t;
 
-typedef struct cb_motor_command_packet_t {
+typedef struct __attribute__((packed)) {
 	uint8_t header; // CB_MOTOR_COMMAND
 	uint8_t automatic;
 	uint8_t steering_pwm;
 	uint8_t drive_pwm;
-} cb_motor_command_packet_t __attribute__((packed));
+} cb_motor_command_packet_t;
 
 #endif

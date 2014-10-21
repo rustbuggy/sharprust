9293c8de5e106ac3cc4479b5f92779646950a03e
Mattis Marjak <mattis.marjak@defendec.com>
2014-10-21 22:52:34 +0300

    sweep test: simulate dead end with length and width of 94

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,5 @@
 #include "stdio.h"
+#include "math.h"
 #include "mcdriver.h"
 #include "communication.h"
 
@@ -11,19 +12,23 @@ int main() {
     telemetry.ir_right       = 0;
     telemetry.ir_front_left  = 0;
     telemetry.ir_front_right = 0;
-    telemetry.ir_front       = 5;
+    telemetry.ir_front       = 94;
 
     for ( int val = 0; val<95; val++ ) {
         telemetry.time += 10;
-        telemetry.ir_left  =     val;
-        telemetry.ir_right = 94-val;
+        telemetry.ir_left           =     val;
+        telemetry.ir_right          =  94-val;
+        telemetry.ir_front_left     = sqrt(2)*telemetry.ir_left;
+        telemetry.ir_front_right    = sqrt(2)*telemetry.ir_right;
         drive_cmd_t& dc = driver.drive(telemetry);
 
-        printf("l %2d r %2d steer %3d drive %3d\n", 
+        printf("l %2d r %2d fl %3d fr %3d steer %3d drive %3d\n", 
             telemetry.ir_left,
             telemetry.ir_right,
-            dc.steeringPwm,
-            dc.drivingPwm 
+            telemetry.ir_front_left,
+            telemetry.ir_front_right,
+            dc.steeringPwm-90,
+            dc.drivingPwm-90 
         );
     }
 

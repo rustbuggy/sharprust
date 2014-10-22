f307e856ae0780bc229f2b2561675f226a010a4e
Timo Tomson <timo.tomson@defendec.com>
2014-10-22 12:13:26 +0300

    when IR sensor outputs low values, always assume "infinity", i.e. 100

--- utils/create_sharp_ir_lookuptable.m
+++ utils/create_sharp_ir_lookuptable.m
@@ -7,21 +7,24 @@ clc
 % Sharp IR sensor 10..80 cm
 lookup = zeros(1,1024);
 for i = 1:1024
-    if i > 112
+    %if i > 112
         voltFromRaw = (i - 1) * (3299 / 1023) + 1;
         cm = 27.728 * ((voltFromRaw / 1000) ^ (-1.2045));
         %if ((cm < 6) || (cm > 80))
         %    lookup(1,i) = 100; % to integer
         %else
         lookup(1,i) = fix(cm); % to integer
+        if lookup(1,i) > 100
+            lookup(1,i) = 100;
+        end
         %end
-    else
-        lookup(1,i) = fix((3 / 112) * (i-1));
-    end
+    %else
+    %    lookup(1,i) = fix((3 / 112) * (i-1));
+    %end
 end
 
 % create the lookup source file
-fid = fopen('lookups.cpp', 'w');
+fid = fopen('../firmware/lookups.cpp', 'w');
 fprintf(fid, '#include "lookups.h"\n\n');
 
 fprintf(fid, 'const uint8_t irLookup[1024] = {\n');
--- utils/mc.m
+++ utils/mc.m
@@ -3,6 +3,27 @@ clc
 clear all
 close all
 
+L = 10
+x1 = 0; y1 = 0;
+x2 = 10; y2 = 10;
+x3 = x2 + L; y3 = y2;
+
+%z = 2*(x1*y2 + x3*y1 + x2*y3 - (x3*y2 + x2*y1 + x1*y3))
+%h = ((x1^2+y1^2)*y2 + (x3^2+y3^2)*y1 + (x2^2+y2^2)*y3 - ((x3^2+y3^2)*y2 + (x2^2+y2^2)*y1 + (x1^2+y1^2)*y3)) / z
+
+mr = (y2-y1)/(x2-x1)
+mt = (y3-y2)/(x3-x2)
+
+x = (mr*mt*(y3-y1) + mr*(x2+x3) - mt*(x1+x2)) / 2*(mr-mt)
+y = -1/mr*(x-(x1+x2)/2) + (y1+y2)/2
+R = sqrt(x^2 + y^2)
+
+alpha1 = radtodeg(atan(L/R))
+alpha2 = radtodeg(atan2(y2, x2))
+alpha1 / alpha2
+
+%{
+
 %l = 15
 %r = 18
 %fl = 59.21
@@ -27,6 +48,7 @@ mc_y = (a1 + a2) / 4
 
 dist = sqrt(mc_x^2 + mc_y^2)
 angle = 57.2957795 * atan2(mc_y, mc_x)
+%}
 
 %{
 a1 = sqrt(1/2) * fr
